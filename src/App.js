59ce9ccdb78a4d694a89af2a7e84104c0033130a
Tyler Sanders <tsanders5902@gmail.com>
2023-03-20 21:33:38 -0400

    Centered everything and moved the take damage button to the right side

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useCallback} from 'react';
+import HealthBar from './components/healthbar';
 
-function App() {
+const App = () => {
+  const [health, setHealth] = useState(40);
+
+  const handleClick = useCallback(() => {
+    setHealth((prevHealth) => Math.max(prevHealth - 5, 0));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', width: '300px', margin: '0 auto' }}>
+      <HealthBar health={health} />
+      <button onClick={handleClick}>Take Damage</button>     
     </div>
   );
-}
+};
 
 export default App;
