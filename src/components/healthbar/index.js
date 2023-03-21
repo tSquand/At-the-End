7565e2c7e18199f103636181378522ee93115fc4
Tyler Sanders <tsanders5902@gmail.com>
2023-03-20 21:31:23 -0400

    Centered everything and moved the take damage button

--- src/components/healthbar/index.js
+++ src/components/healthbar/index.js
@@ -6,14 +6,13 @@ const HealthBar = ({health}) => {
     const healthPercentage = Math.max(remainingHealth / maxHealth) * 100;
 
     return (
-        <div style={{backgroundColor: '#eee', height: '25px', width: '200px', border: "1px solid black", position: 'relative'}}>
-            <div style={{backgroundColor: 'red', height: '100%', width: `${healthPercentage}%`}}>
-                <div style={{position: 'absolute', top: 0, left: 0, right: 0, bottom: 0, display: 'flex', alignItems: 'center', justifyContent: 'center'}}
-                >
-                    <span style={{ color: "black", fontWeight: "bold" }}>
-                        {remainingHealth} / {maxHealth} 
-                        </span>                    
-                </div>
+        <div style={{ display: 'flex', justifyContent: 'center', backgroundColor: '#eee', height: '25px', width: '200px', border: "1px solid black", position: 'relative'}}>
+            <div style={{backgroundColor: 'red', height: '100%', width: `${healthPercentage}%`, position: 'absolute', top: 0, left: 0 }}>
+            </div>
+            <div style={{position: 'absolute', top: 0, left: 0, right: 0, bottom: 0, display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+                <span style={{ color: "black", fontWeight: "bold" }}>
+                    {remainingHealth} / {maxHealth} 
+                </span>                    
             </div>
         </div>
     );
