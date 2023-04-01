d0ccb3b77623ad9f14a624600d39fe742550406f
Tyler Sanders <tsanders5902@gmail.com>
2023-04-01 18:25:32 -0400

    Added death message

--- src/components/healthbar/index.js
+++ src/components/healthbar/index.js
@@ -4,6 +4,10 @@ const HealthBar = ({health, maxHealth}) => {
     const remainingHealth = Math.max(0, health); 
     const healthPercentage = (remainingHealth / maxHealth) * 100;
     
+    let displayText = `${remainingHealth} / ${maxHealth}`;
+    if (remainingHealth === 0) {
+        displayText = "You're dead";
+    }
 
     return (
         <div style={{ display: 'flex', justifyContent: 'center', backgroundColor: 'white', height: '50px', width: '200px', border: "1px solid black", position: 'relative'}}>
@@ -11,7 +15,7 @@ const HealthBar = ({health, maxHealth}) => {
             </div>
             <div style={{position: 'absolute', top: 0, left: 0, right: 0, bottom: 0, display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
                 <span style={{ color: "black", fontWeight: "bold" }}>
-                    {remainingHealth} / {maxHealth} 
+                    {displayText} 
                 </span>                    
             </div>
         </div>
